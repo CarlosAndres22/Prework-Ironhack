4ebc1ac7665994ef39b8697b31cabf5e850f04f1
Andres <tizam15@hotmail.com>
2016-01-04 15:38:39 -0500

    modificado

--- ruby/clases.rb
+++ ruby/clases.rb
@@ -10,22 +10,5 @@ puts my_car.class  #Metodo CLASS es definir que clase es
 puts "cosa".class
 puts 4.class
 puts "======================================"
-class Car
-  def initialize(color)
-    @color = color
-  end
 
-  def honk
-    puts "Beeeeeeeeep!"
-  end
-
-  def print_color
-    puts @color
-  end
-end
-
-my_car = Car.new "red"
-other_car = Car.new "grey"
-
-my_car.print_color
-other_car.print_color
+puts "hello"
